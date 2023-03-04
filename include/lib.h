697e5c6735b24e5637f28232e4e221c194384b2e
gogoex <110195520+gogoex@users.noreply.github.com>
2023-03-04 11:24:55 +0900

    add python swig

---
+++ include/lib.h
@@ -0,0 +1,7 @@
+#ifndef BLSCT_LIB_LIB_H
+#define BLSCT_LIB_LIB_H
+
+void BlsInit();
+int TestAddition();
+
+#endif // BLSCT_LIB_LIB_H
--- src/include/lib.h
+++
@@ -1,7 +0,0 @@
-#ifndef BLSCT_LIB_SRC_LIB_H
-#define BLSCT_LIB_SRC_LIB_H
-
-void BlsInit();
-int TestAddition();
-
-#endif // BLSCT_LIB_SRC_LIB_H
