64ba4f2a439dd330847754508765158fa276b6e1
gogoex <110195520+gogoex@users.noreply.github.com>
2023-03-12 10:48:16 +0900

    use shared object for rust ffi

--- ffi/rust/README.md
+++ ffi/rust/README.md
@@ -1,18 +1,7 @@
 ## Building & Running
 ```
-$ cargo run
+$ LD_LIBRARY_PATH=../../src/bls/mcl/lib cargo run
 ```
 
 ## TODO
-- when i run the lib with `.compile("blsct")` for the first time, it fails.
-then if i change it to `.compile("blsct2")` and run again, it builds fine.
-- however if i start with `.compile("blsct.so")` and do `.compile("blsct"), it doesn't work
-- build dir after `.compile("blsct")` is:
-  ```
-  bindings.rs  e58908e91188c193-lib.o  libblsct.a
-  ```
-- build dir after running `.compile("blsct.so")` it becomes:
-  ```
-  bindings.rs  e58908e91188c193-lib.o  libblsct.a  libblsct.so.a
-  ```
-- if `blsct.so` is replaced with `blsct2`, it still works
+- fix the problem of rpath for mcl lib is not used to load `libmcl.so` and unneccesitate `LD_LIBRARY_PATH`
--- ffi/rust/build.rs
+++ ffi/rust/build.rs
@@ -5,59 +5,48 @@ fn main() {
     let repo_root = &PathBuf::from("../..");
     let src_dir = &repo_root.join("src");
     let include_dir = &repo_root.join("include");
-    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let out_dir = &PathBuf::from(env::var("OUT_DIR").unwrap());
+    let bls_dir = &src_dir.join("bls");
+    let mcl_dir = &bls_dir.join("mcl");
+    let bls_lib_dir = bls_dir.join("lib");
+    let mcl_lib_dir = mcl_dir.join("lib");
 
     // -L
     println!("cargo:rustc-link-search={}", out_dir.to_str().unwrap());
     println!("cargo:rustc-link-search={}", src_dir.to_str().unwrap());
-    println!("cargo:rustc-link-search={}/bls/lib", src_dir.to_str().unwrap());
-    println!("cargo:rustc-link-search={}/bls/mcl/lib", src_dir.to_str().unwrap());
+    println!("cargo:rustc-link-search={}", bls_lib_dir.to_str().unwrap());
+    println!("cargo:rustc-link-search={}", mcl_lib_dir.to_str().unwrap());
 
     // -l
     println!("cargo:rustc-link-lib=blsct");
     println!("cargo:rustc-link-lib=bls384_256");
     println!("cargo:rustc-link-lib=mcl");
-    println!("cargo:rustc-link-lib=stdc++");
 
-    // header file to create bindings
+    // -rpath; $ORIGIN expands to the path to the executable
+    println!("cargo:rustc-link-arg=-Wl,-rpath=$ORIGIN/../../../../src/bls/lib");
+    println!("cargo:rustc-link-arg=-Wl,-rpath=$ORIGIN/../../../../src/bls/mcl/lib");
+
     println!("cargo:rerun-if-changed={}/lib.cpp", src_dir.to_str().unwrap());
     println!("cargo:rerun-if-changed={}/lib.h", include_dir.to_str().unwrap());
 
+    let blsctlib = "libblsct.so";
     if !std::process::Command::new("g++")
-        .arg("-c")
+        //.arg("-Wl,-rpath,/foo")
+        .arg("-fPIC")
+        .arg("-shared")
         .arg("-I").arg(src_dir.join("bls").join("include"))
         .arg("-I").arg(src_dir.join("bls").join("mcl").join("include"))
         .arg("-I").arg(repo_root)
         .arg("-I").arg(repo_root.join("include"))
-        .arg("-o").arg(out_dir.join("blsct.o"))
         .arg(src_dir.join("lib.cpp"))
+        .arg("-o").arg(out_dir.join(blsctlib))
         .output()
         .expect("Failed to spawn `g++`")
         .status
         .success()
     {
-        panic!("Failed to build lib.o");
+        panic!("Failed to build {}", blsctlib);
     }
-    if !std::process::Command::new("ar")
-        .arg("rcs")
-        .arg(out_dir.join("libblsct.a"))
-        .arg(out_dir.join("blsct.o"))
-        .output()
-        .expect("Failed to spawn `ar`")
-        .status
-        .success()
-    {
-        panic!("Failed to build libblsct.a");
-    }
-
-    // cc::Build::new()
-    //     .cpp(true)
-    //     .file(src_dir.join("lib.cpp"))
-    //     .include(src_dir.join("bls").join("include"))
-    //     .include(src_dir.join("bls").join("mcl").join("include"))
-    //     .include(repo_root)
-    //     .include(repo_root.join("include"))
-    //     .compile("blsct");
 
     let bindings = bindgen::Builder::default()
         .header(include_dir.join("lib.h").to_str().unwrap())
